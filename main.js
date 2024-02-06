7157cee79abcb7f3b9a54d9e40916951bc1cace0
yanadiii <152841571+yanadiii@users.noreply.github.com>
2024-02-06 21:57:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("btn1");
+alert("sulap by ADI MAHA");
+let input = prompt("pikirkan 2 angka, saya akan menebaknya");
+alert("angka yang anda pikirkan adalah " + input);
+
+function gantiWarna() {
+  const h1 = document.createElement("h1ne");
+  h1.innerHTML = " jancok lu ";
+  body.append(h1);
+}
